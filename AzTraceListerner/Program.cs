88d567a12cdbde29add0c0bb1e106747f2d76e01
arpitfs <amarpitmalik204@gmail.com>
2020-11-20 00:27:00 +0530

    integrate application insights with existing application

---
+++ AzTraceListerner/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TelemetryConfiguration.Active.InstrumentationKey = "INSTRUMENTATION_KEY";
+            var client = new TelemetryClient();
+            client.TrackTrace("Demo application starting up.");
+            Debug.WriteLine("Other log");
+            client.Flush();
+        }
+    }
+}
